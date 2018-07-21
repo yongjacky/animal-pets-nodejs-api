ab80f28596b28bccd673320799b5ec8922c72a5a
Jacky Yong <jacky@lesofi.asia>
2018-07-21 20:08:00 +0800

    First basecodes commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import morgon from 'morgan'
+import path from 'path'
+import fs from 'fs'
+import rfs from 'rotating-file-stream'
+
+const app = express()
+const homeRoute = express.Router()
+const api = express.Router()
+const serverPort = process.env.port || 3000
+const logDirectory = path.join(__dirname,'../logs')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const accessLogStream = rfs('access.log',{
+    size: '10M',
+    interval: '1d',
+    path: logDirectory,
+    compress: 'gzip'
+})
+
+app.use(morgon('combined',{stream: accessLogStream}))
+app.use('/api',api)
+app.use('/',homeRoute)
+
+homeRoute.get('/',(req,res)=>{
+    res.status(200).json({message: 'Welcome Animal Pets API Endpoints'})
+})
+
+app.listen(serverPort,()=>{
+    console.log('Animal Pets API server started!!')
+})
